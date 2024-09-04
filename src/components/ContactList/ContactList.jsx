d8fc05a47672ca26901df480baa62ac02b564dd9
morce-dev <morley@morce.dev>
2024-09-05 04:17:20 +0800

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,9 @@
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import PropTypes from 'prop-types';
 
 export const ContactList = ({ filterContact, deleteContact }) => {
   const filteredContacts = filterContact();
+
   return (
     <ul>
       {filteredContacts.map(filteredContact => (
@@ -13,4 +15,9 @@ export const ContactList = ({ filterContact, deleteContact }) => {
       ))}
     </ul>
   );
+};
+
+ContactList.propTypes = {
+  filterContact: PropTypes.func.isRequired,
+  deleteContact: PropTypes.func.isRequired,
 };
